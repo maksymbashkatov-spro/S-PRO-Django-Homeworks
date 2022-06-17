30d52d4052995a4ba2ce1d3a40730f29bc516572
Максим Башкатов <maksymbashkatov.spro@gmail.com>
2022-06-17 14:59:29 +0300

    Was finished hw01_task3.

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from firstapp.views import hellodjango, greeting
+from firstapp.views import hellodjango, greeting, get_date, get_concrete_date
 
 urlpatterns = [
     path('', hellodjango),
-    path('<str:name>', greeting)
+    path('<str:name>', greeting),
+    path('date/', get_date),
+    path('date/<str:date_format>', get_concrete_date)
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+import datetime
+
+
+dd = datetime.date.today().strftime('%d.%m.%Y')
+dd_lst = tuple(dd.split('.'))
 
 
 def hellodjango(request: HttpRequest):
@@ -7,3 +12,18 @@ def hellodjango(request: HttpRequest):
 
 def greeting(request: HttpRequest, name):
     return HttpResponse(f'Hello {name}!')
+
+
+def get_date(request: HttpRequest):
+    return HttpResponse(dd)
+
+
+def get_concrete_date(request: HttpRequest, date_format):
+    if date_format == 'year':
+        return HttpResponse(dd_lst[2])
+    elif date_format == 'month':
+        return HttpResponse(dd_lst[1])
+    elif date_format == 'day':
+        return HttpResponse(dd_lst[0])
+    else:
+        return HttpResponseNotFound(f'Date format {date_format} is not found.')
