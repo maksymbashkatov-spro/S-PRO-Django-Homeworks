60f08c89eb667f3e2da05014ee5bf50d8726cc2e
Максим Башкатов <maksymbashkatov.spro@gmail.com>
2022-07-03 23:40:09 +0300

    Was finished hw02_task4.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from bookstore.views import all_books, full_desc
+from bookstore.views import all_books, full_desc, get_book, get_author
 
 urlpatterns = [
-    path('', all_books),
-    path('desc/<int:id>', full_desc, name='full_desc')
+    path('', all_books, name='all_books'),
+    path('desc/<int:id>', full_desc, name='full_desc'),
+    path('book/<int:id>', get_book),
+    path('author/<int:id>', get_author, name='get_author')
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -9,33 +9,36 @@ books = [{'id': 1,
                          'effective, idiomatic Python code by leveraging its best—and possibly most '
                          'neglected—features. Author Luciano Ramalho takes you through Python’s core language features '
                          'and libraries, and shows you how to make your code shorter, faster, and more readable '
-                         'at the same time.', 'author_id': 1},
+                         'at the same time.',
+          'author_id': 1},
          {'id': 2,
           'title': 'title2',
           'released_year': 2001,
           'description': 'Description2description2description2description2description2description2. '
-                         'Description2description2description2description2description2description2.'},
+                         'Description2description2description2description2description2description2.',
+          'author_id': 2},
          {'id': 3,
           'title': 'title3',
           'released_year': 2010,
           'description': 'Description3description3description3description3description3description3. '
-                         'Description3description3description3description3description3description3.'},
+                         'Description3description3description3description3description3description3.',
+          'author_id': 1},
          {'id': 4,
           'title': 'title4',
           'released_year': 2011,
           'description': 'Description4description4description4description4description4description4. '
-                         'Description4description4description4description4description4description4.'},
+                         'Description4description4description4description4description4description4.',
+          'author_id': 3},
          {'id': 5,
           'title': 'title5',
           'released_year': 2015,
           'description': 'Description5description5description5description5description5description5. '
-                         'Description5description5description5description5description5description5.'}]
+                         'Description5description5description5description5description5description5.',
+          'author_id': 2}]
 
 authors = [{'id': 1, 'first_name': 'Luciano', 'last_name': 'Ramalho', 'age': 51},
-           {'id': 1, 'first_name': 'FirstName2', 'last_name': 'LastName2', 'age': 40},
-           {'id': 1, 'first_name': 'FirstName3', 'last_name': 'LastName3', 'age': 37},
-           {'id': 1, 'first_name': 'FirstName4', 'last_name': 'LastName4', 'age': 29},
-           {'id': 1, 'first_name': 'FirstName5', 'last_name': 'LastName5', 'age': 43}]
+           {'id': 2, 'first_name': 'FirstName2', 'last_name': 'LastName2', 'age': 40},
+           {'id': 3, 'first_name': 'FirstName3', 'last_name': 'LastName3', 'age': 37}]
 
 
 def all_books(request: HttpRequest):
@@ -44,3 +47,11 @@ def all_books(request: HttpRequest):
 
 def full_desc(request: HttpRequest, id):
     return render(request, 'bookstore/full_desc.html', {'description': books[id - 1]['description']})
+
+
+def get_book(request: HttpRequest, id):
+    return render(request, 'bookstore/get_book.html', {'book': books[id - 1]})
+
+
+def get_author(request: HttpRequest, id):
+    return render(request, 'bookstore/get_author.html', {'author': authors[id - 1]})
