e0f9f464cbee9b9409aa7f199caae26114bc8ee1
Максим Башкатов <maksymbashkatov.spro@gmail.com>
2022-07-08 16:23:01 +0300

    Was finished hw03_task1.

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'authors'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    released_year = models.DateField()
+    description = models.TextField()
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'books'
